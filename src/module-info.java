3ae8ccd5a3c508cc68165d6d0aaa95dc417341c9
arapiermal <109100661+arapiermal@users.noreply.github.com>
2025-02-06 05:52:00 +0100

    Delete src directory

--- src/module-info.java
+++
@@ -1,7 +0,0 @@
-module ErmalArapiMidtermAOOP {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires java.sql;
-	opens application to javafx.graphics, javafx.fxml, javafx.base;
-}
